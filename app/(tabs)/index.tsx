47fb0bf5fabc3636b49639dcc12e09a459f97cd4
seyitbugraerden <seyitbugraerden@gmail.com>
2024-08-21 01:14:18 +0300

    flatlist kurulumu

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,35 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, Text, FlatList } from "react-native";
 
 export default function HomeScreen() {
+  const courses = [
+    { name: "Angular", id: 1 },
+    { name: "React Js", id: 2 },
+    { name: "C#", id: 3 },
+    { name: "C Programlama", id: 4 },
+    { name: "Bootstrap", id: 5 },
+  ];
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>React Native</Text>
-    </View>
+    <FlatList
+      data={courses} //Verileri çekiyoruz
+      // horizontal={true} Yatay şekilde uzatır
+      // showsHorizontalScrollIndicator={false} //scroll bar görüntüsünü ortadan kaldırır.
+      keyExtractor={(item: any) => item.id} //her bir elemanın özel olduğunu belirtmemiz gerek.
+      renderItem={({ item }) => { //Çekin verileri map'liyoruz
+        return (
+          <Text style={styles.content}>
+            {item.name} / {item.id}
+          </Text>
+        );
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000",
-  },
-  text: {
-    color: "white",
+  content: {
+    fontSize: 20,
+    backgroundColor: "yellow",
+    marginVertical: 10,
+    padding: 20,
   },
 });
