267e1b5a4b5f1d765205bb94963586aca2546082
seyitbugraerden <seyitbugraerden@gmail.com>
2024-08-23 15:18:31 +0300

    navigation starter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,12 @@
-import { StyleSheet, Text, FlatList } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default function HomeScreen() {
-  const courses = [
-    { name: "Angular", id: 1 },
-    { name: "React Js", id: 2 },
-    { name: "C#", id: 3 },
-    { name: "C Programlama", id: 4 },
-    { name: "Bootstrap", id: 5 },
-  ];
+const HomeScreen = () => {
   return (
-    <FlatList
-      data={courses} //Verileri çekiyoruz
-      // horizontal={true} Yatay şekilde uzatır
-      showsHorizontalScrollIndicator={false} //scroll bar görüntüsünü ortadan kaldırır.
-      keyExtractor={(item: any) => item.id} //her bir elemanın özel olduğunu belirtmemiz gerek.
-      renderItem={({ item }) => { //Çekin verileri map'liyoruz
-        return (
-          <Text style={styles.content}>
-            {item.name} / {item.id}
-          </Text>
-        );
-      }}
-    />
-  );
+    <View>
+      <Text>index</Text>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  content: {
-    fontSize: 20,
-    backgroundColor: "yellow",
-    marginVertical: 10,
-    padding: 20,
-  },
-});
+export default HomeScreen
